b1454420749442c97023ad6aba5f50280ada0ca5
Harry Johnson <harrycj@msn.com>
2019-01-18 11:53:26 +0000

    projects

--- HJWebDevSite/home/models.py
+++ HJWebDevSite/home/models.py
@@ -6,6 +6,9 @@ from wagtail.core import blocks
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.images.blocks import ImageChooserBlock
 from HJWebDevSite.blocks import HeadingBlock, ParagraphBlock, ThreeColumnBlock, ButtonBlock, GridColumnBlockCollection, ContactBlock
+from home.forms import ContactForm
+from django.core import mail
+from django.contrib import messages
 
 
 class HomePage(Page):
@@ -23,6 +26,20 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         StreamFieldPanel('body'),
     ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+
+        # TODO: Check if page contains a contact form
+        if context['request'].method == "POST":
+            contact_form = ContactForm(context['request'].POST)
+            if contact_form.is_valid():
+                message = "name: "+contact_form.cleaned_data["name"]+'\n'+'email: '+contact_form.cleaned_data["email"]+'\nmessage:\n'+contact_form.cleaned_data["message"]
+                messages.add_message(context['request'], messages.SUCCESS, 'Message sent successfully.')
+                m = mail.EmailMessage("new mesage from hjwebdev", message, 'noreply@hjwebdev.co.uk', ["hjwebdev@googlemail.com"])
+                m.send()
+
+        return context
 
     class Meta:
         verbose_name = "page"
